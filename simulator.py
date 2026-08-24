d5f034359aef823b1e4a65717ee1458fe8ec3d50
Gurleen Singh <singhgurleen551@gmail.com>
2026-08-24 17:07:55 +0100

    Initial N-body simulator setup

---
+++ simulator.py
@@ -0,0 +1,17 @@
+def main():
+    variables()
+
+
+def variables():
+    name1, name2 = input("Name of the body 1 and bosy 2: ")
+    mass1, mass2 = float(input("Mass of the body 1 and bosy 2: "))
+    x1, y1, x2, y2 = float(input("The intial x and y position of the body 1 and body 2"))
+    vx1, vy1, vx2, vy2 = float(input("The initial x and y velocity of the body 1 and body 2: "))
+
+
+
+
+
+
+main()
+ 
