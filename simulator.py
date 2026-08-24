4982033ee1490f91471b67939702f47f8bacae69
Gurleen Singh <singhgurleen551@gmail.com>
2026-08-24 21:35:43 +0100

    Class made

--- simulator.py
+++ simulator.py
@@ -1,12 +1,23 @@
+class Bodies:
+    def __init__(self, name, mass, x, y, vx, vy):
+        self.name = name
+        self.mass = mass
+        self.position = x, y
+        self.velocity = vx, vy
+
+
+
 def main():
     variables()
 
 
 def variables():
-    name1, name2 = input("Name of the body 1 and bosy 2: ")
-    mass1, mass2 = float(input("Mass of the body 1 and bosy 2: "))
-    x1, y1, x2, y2 = float(input("The intial x and y position of the body 1 and body 2"))
-    vx1, vy1, vx2, vy2 = float(input("The initial x and y velocity of the body 1 and body 2: "))
+    for _ in range(0,2):
+        name = input("Name of the body: ")
+        mass = float(input("Mass of the body: "))
+        position = float(input("The intial x and y position of the body: "))
+        velocity = float(input("The initial x and y velocity of the body: "))
+        return Bodies(name, mass, position, velocity)
 
 
 
