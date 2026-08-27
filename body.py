f1f4f5a5aa149f61232ccd22d4ff0508c7afc58e
Gurleen Singh <singhgurleen551@gmail.com>
2026-08-27 19:42:01 +0100

    file names

---
+++ body.py
@@ -0,0 +1,12 @@
+class Bodies:
+    def __init__(self, name, mass, x, y, vx, vy):
+        self.name = name
+        self.mass = mass
+        self.position = x, y
+        self.velocity = vx, vy
+
+
+
+
+
+
