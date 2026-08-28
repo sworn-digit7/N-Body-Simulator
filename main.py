da6aa1ce1114181294c60012f429a648ac3531b0
Gurleen Singh <singhgurleen551@gmail.com>
2026-08-28 14:34:45 +0100

    successfully works with earth & sun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from body import Body
 from simulation import Simulation
+import matplotlib.pyplot as plt
 
 sun = Body(
     name = "sun",
@@ -21,7 +22,31 @@ simulation = Simulation(
 )
 
 positions = simulation.run(8760)
+earth_x = [position[1][0] for position in positions]
+earth_y = [position[1][1] for position in positions]
+
+sun_x = [position[0][0] for position in positions]
+sun_y = [position[0][1] for position in positions]
 
 # print(len(positions))
 # print(positions[0])
 # print(positions[-1])
+
+# x position
+# against
+# y position
+
+# title
+# x-axis label
+# y-axis label
+# legend
+# grid
+plt.plot(earth_x, earth_y, label="Earth")
+plt.plot(sun_x, sun_y, label="Sun")
+
+plt.title("Earth's Orbit Around the Sun")
+plt.xlabel("x position (m)")
+plt.ylabel("y position (m)")
+plt.legend()
+plt.grid()
+plt.show()
