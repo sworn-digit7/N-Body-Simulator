86266ac4131ce243173412e3556fe2a769710018
Gurleen Singh <singhgurleen551@gmail.com>
2026-08-28 12:35:33 +0100

    added updating feature

--- simulation.py
+++ simulation.py
@@ -3,7 +3,7 @@ import math
 
 class Simulation:
     def __init__(self, bodies, timestep):
-        self.name = bodies
+        self.bodies = bodies
         self.timestep = timestep
         
 # F = G m₁m₂ / r²
@@ -50,6 +50,37 @@ class Simulation:
 
         return a_x, a_y
 
-    # def calculate_total_acceleration(self, body):
-    #     total_a_x = 0
-    #     total_a_y = 0
+    def calculate_total_acceleration(self, body):
+        total_a_x = 0
+        total_a_y = 0
+
+        for other_body in self.bodies:
+
+            if body != other_body:
+                a, b = self.calculate_acceleration(body, other_body)
+                total_a_x += a
+                total_a_y += b
+
+
+            return total_a_x, total_a_y
+
+# vx_new = vx_old + ax × dt
+
+# vy_new = vy_old + ay × dt
+
+    def update_velocity(self, body):
+
+        ax, ay = self.calculate_total_acceleration(body)
+
+        new_vx = body.velocity[0] + ax * self.timestep
+        new_vy = body.velocity[1] + ay * self.timestep
+
+        return new_vx, new_vy
+
+    def update_position(self, body):
+
+        new_px = body.position[0] + body.velocity[0] * self.timestep
+        new_py = body.position[1] + body.velocity[1] * self.timestep
+
+        return new_px, new_py
+
